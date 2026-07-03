6e7b2c0e209f08ea6291701c90bad5850bf880f8
Oleksandr <zombozo223@gmail.com>
2026-07-03 13:25:13 +0300

    new-update-5

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,15 @@
+import type { ComponentType } from "react";
+import ReactPaginateModule from "react-paginate";
+import type { ReactPaginateProps } from "react-paginate";
 import css from "./Pagination.module.css";
+
+type ModuleWithDefault<T> = { default: T };
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<
+    ComponentType<ReactPaginateProps>
+  >
+).default;
 
 interface PaginationProps {
   currentPage: number;
@@ -11,31 +22,15 @@ export const Pagination = ({
   totalPages,
   onPageChange,
 }: PaginationProps) => {
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
-
   return (
-    <ul className={css.pagination}>
-      <li onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}>
-        <a>Previous</a>
-      </li>
-
-      {pages.map((page) => (
-        <li
-          key={page}
-          className={page === currentPage ? css.active : ""}
-          onClick={() => onPageChange(page)}
-        >
-          <a>{page}</a>
-        </li>
-      ))}
-
-      <li
-        onClick={() =>
-          currentPage < totalPages && onPageChange(currentPage + 1)
-        }
-      >
-        <a>Next</a>
-      </li>
-    </ul>
+    <ReactPaginate
+      forcePage={currentPage - 1}
+      pageCount={totalPages}
+      onPageChange={(event) => onPageChange(event.selected + 1)}
+      previousLabel={"Previous"}
+      nextLabel={"Next"}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+    />
   );
 };
